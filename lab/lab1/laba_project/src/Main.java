4576a89f0ab7560fd6832e8ba6780c29f47f8413
Тема Александров <maynix@MacBook-Pro-Tema.local>
2025-10-04 06:52:45 +0300

    test on mac

--- lab/lab1/laba_project/src/Main.java
+++ lab/lab1/laba_project/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("0) Основное задание.");
+        System.out.println("0) Основное задание. ");
         MainTask.main(args);
         System.out.println();
 
