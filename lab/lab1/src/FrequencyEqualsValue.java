fe3df99a2e8f9802008bd8ca71ada3cf623595a2
oadianova <olesyadianov@yandex.ru>
2025-10-13 19:27:55 +0300

    Update FrequencyEqualsValue.java

--- lab/lab1/src/FrequencyEqualsValue.java
+++ lab/lab1/src/FrequencyEqualsValue.java
@@ -4,24 +4,9 @@
 
 import java.util.Scanner;
 
-public class FrequencyEqualsValue {
-
-    public static int findLuckyNumber(int[] numbers) {
-        int maxLuckyNumber = -1;
-
-        for (int i = 0; i < numbers.length; i++) {
-            int currentNumber = numbers[i];
-            int frequency = countFrequency(numbers, currentNumber);
-
-            if (isLuckyNumber(currentNumber, frequency) && currentNumber > maxLuckyNumber) {
-                maxLuckyNumber = currentNumber;
-            }
-        }
-
-        return maxLuckyNumber;
-    }
-
-    private static int countFrequency(int[] numbers, int target) {
+public class ArrayUtils {
+    
+    public static int countFrequency(int[] numbers, int target) {
         int count = 0;
         for (int number : numbers) {
             if (number == target) {
@@ -31,9 +16,15 @@ public class FrequencyEqualsValue {
         return count;
     }
 
-    private static boolean isLuckyNumber(int number, int frequency) {
-        return number == frequency;
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
+}
+
+public class FrequencyEqualsValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -51,5 +42,24 @@ public class FrequencyEqualsValue {
         System.out.println("Result: " + result);
 
         scanner.close();
+    }
+
+    public static int findLuckyNumber(int[] numbers) {
+        int maxLuckyNumber = -1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int currentNumber = numbers[i];
+            int frequency = ArrayUtils.countFrequency(numbers, currentNumber);
+
+            if (isLuckyNumber(currentNumber, frequency) && currentNumber > maxLuckyNumber) {
+                maxLuckyNumber = currentNumber;
+            }
+        }
+
+        return maxLuckyNumber;
+    }
+
+    private static boolean isLuckyNumber(int number, int frequency) {
+        return number == frequency;
     }
 }
