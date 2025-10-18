94a0cfa3e6d5a4892124877750456350ae96f72e
oadianova <olesyadianov@yandex.ru>
2025-10-18 11:33:14 +0300

    Update FrequencyEqualsValue.java

--- lab/lab1/src/FrequencyEqualsValue.java
+++ lab/lab1/src/FrequencyEqualsValue.java
@@ -4,62 +4,29 @@
 
 import java.util.Scanner;
 
-public class FrequencyEqualsValue {
-
+public class SumTwoSmallestPositive {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter array size: ");
         int size = scanner.nextInt();
 
-        int[] numbers = new int[size];
-        System.out.println("Enter array elements:");
-        for (int i = 0; i < size; i++) {
-            numbers[i] = scanner.nextInt();
+        if (size < 5) {
+            System.out.println("Error: array must contain at least 5 elements");
+            scanner.close();
+            return;
         }
 
-        int result = findLuckyNumber(numbers);
+        int[] array = new int[size];
+        System.out.println("Enter " + size + " elements:");
+
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int result = ArrayProcessor.calculateSumTwoSmallestPositive(array);
         System.out.println("Result: " + result);
 
         scanner.close();
-    }
-
-    public static int findLuckyNumber(int[] numbers) {
-        int maxLuckyNumber = -1;
-
-        for (int i = 0; i < numbers.length; i++) {
-            int currentNumber = numbers[i];
-            int frequency = ArrayUtils.countFrequency(numbers, currentNumber);
-
-            if (isLuckyNumber(currentNumber, frequency) && currentNumber > maxLuckyNumber) {
-                maxLuckyNumber = currentNumber;
-            }
-        }
-
-        return maxLuckyNumber;
-    }
-
-    private static boolean isLuckyNumber(int number, int frequency) {
-        return number == frequency;
-    }
-}
-
-class ArrayUtils {  
-
-    public static int countFrequency(int[] numbers, int target) {
-        int count = 0;
-        for (int number : numbers) {
-            if (number == target) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
     }
 }
