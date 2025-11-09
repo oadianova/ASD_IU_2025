366eff84704b79ce7b2fd27119a8eeeaecc50c94
oadianova <olesyadianov@yandex.ru>
2025-11-09 18:44:45 +0300

    Update CollectionPerformanceTest.java

--- lab/lab2/src/CollectionPerformanceTest.java
+++ lab/lab2/src/CollectionPerformanceTest.java
@@ -1,36 +1,4 @@
 import java.util.*;
-
-class Student {
-    private Long id;
-    private String name;
-
-    public Student(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(id, student.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-}
-
 public class CollectionPerformanceTest {
 
     public static void main(String[] args) {
